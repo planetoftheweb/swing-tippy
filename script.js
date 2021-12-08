c38c699a8e17e0a198baf9ba5d441564ab239fda
Ray Villalobos <ray@planetoftheweb.com>
2021-12-08 10:41:23 -0500

    finished project

---
+++ script.js
@@ -0,0 +1,12 @@
+tippy('.btn', {
+  duration: 500,
+  placement: 'top',
+  arrow: false,
+  followCursor: 'horizontal',
+  inertia: true,
+  delay: [100, 200],
+  theme: 'orange',
+  animation: 'shift-away',
+  // arrow: false,
+  content: 'My tooltip!',
+});
